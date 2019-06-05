aade195202f79c0572344f9066bd5e876d6d30f9
Seyi MontXero <a.arogbonlo@gmail.com>
2019-06-05 22:38:41 +1000

    removed autogenerated emacs files & folders. They are not portable anyway

--- .emacs.d/elpa/pipenv-20190307.1155/pipenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pipenv" "20190307.1155" "A Pipenv porcelain." '((emacs "25.1") (f "0.19.0") (s "1.12.0") (pyvenv "1.20")) :commit "189cf5a4d425b6bdcd26742a460cf12cc540f524" :authors '(("Paul Walsh" . "paulywalsh@gmail.com")) :maintainer '("Paul Walsh" . "paulywalsh@gmail.com") :url "https://github.com/pwalsh/pipenv.el")
